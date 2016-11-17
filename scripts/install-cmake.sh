3ad9d4d06809ce56dcb4118f5687d2a077a9d0ee
dvirtz <dvirtz@gmail.com>
2016-11-17 23:56:27 +0200

    fix bash script

--- scripts/install-cmake.sh
+++ scripts/install-cmake.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-if [! -d /home/travis/cmake]; then
+
+if [ ! -d /home/travis/cmake ]; then
 	wget --no-check-certificate https://cmake.org/files/v3.2/cmake-3.2.0-Linux-x86_64.sh
 	chmod +x cmake-3.2.0-Linux-x86_64.sh
 	mkdir /home/travis/cmake
