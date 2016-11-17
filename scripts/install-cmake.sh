a17e34eac8e9f214ac1fa174eba0f43013a69708
dvirtz <dvirtz@gmail.com>
2016-11-17 22:59:17 +0200

    add travis build

---
+++ scripts/install-cmake.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+if [! -d /home/travis/cmake]; then
+	wget --no-check-certificate https://cmake.org/files/v3.2/cmake-3.2.0-Linux-x86_64.sh
+	chmod +x cmake-3.2.0-Linux-x86_64.sh
+	mkdir /home/travis/cmake
+	./cmake-3.2.0-Linux-x86_64.sh --skip-license --prefix=/home/travis/cmake
+fi
+export PATH=/home/travis/cmake/bin:${PATH}
