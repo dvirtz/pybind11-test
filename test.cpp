da2b40e2e344b9aaf38cea4f54aa998462305f2f
dvirtz <dvirtz@gmail.com>
2016-11-21 21:23:50 +0200

    use gmusicapi on test

--- test.cpp
+++ test.cpp
@@ -1,4 +1,5 @@
 #include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
 #include <iostream>
 
 int main()
@@ -12,6 +13,10 @@ int main()
         auto sys = py::module::import("sys");
         py::str version(sys.attr("version"));
         py::print(version);
+
+        auto gmusicapi = py::module::import("gmusicapi");
+        version = gmusicapi.attr("__version__");
+        std::cout << "gmusicapi version " << version << '\n';
     }
     catch (py::error_already_set& e)
     {
